49978651db53b0eb6674ba670b79ad9353644c64
Shadeslayer2374 <62838431+Shadeslayer2374@users.noreply.github.com>
2020-08-20 22:11:16 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# ShootingGame1
+The first stage of the Shooting Game I will be creating using Visual Studio Code
---
+++ sketch.js
@@ -0,0 +1,42 @@
+const { Engine, World, Bodies, Body, Mouse, MouseConstraint, Constraint, Composite, Detector } = Matter;
+
+var engine, world;
+var tanker;
+var angle = 0
+var canonBall;
+var shot;
+var ground;
+var ball_1, ball_2, ball_3
+var launcherX, launcherY;
+var flag = "start"
+
+function setup() {
+    var canvas = createCanvas(800, 500);
+    engine = Engine.create();
+    world = engine.world;
+    ground = new Ground(width / 2, height - 10, width, 30);
+    tanker = new Tanker(75, height - 110, 60, 30);
+
+    ball1 = new Ball(400, 50, 20)
+    ball2 = new Ball(500, 100, 20)
+    ball3 = new Ball(600, 150, 20)
+
+    canonball = new CanonBall(20, 20);
+
+
+    shoot = new ShootBall(canonBall.body, { x: 70, y: height - 220 });
+}
+
+function draw() {
+    background(255)
+    Matter.Engine.update(engine);
+    ground.display();
+    fill("red");
+    ball2.display();
+    ball1.display();
+    ball3.display();
+    canonball.display();
+    fill("black");
+    tanker.display();
+    shoot.display();
+}
